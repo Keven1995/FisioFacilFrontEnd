ac283c869650c69f696eb3c82607823b7e15bf94
Keven <lkeven77@gmail.com>
2024-10-13 21:12:06 -0300

    Adicionado Header e Footer

--- Fisio-Facil/src/App.jsx
+++ Fisio-Facil/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <h1>Fisio Facil</h1>
+      <Footer/>
     </div>
   );
 }
