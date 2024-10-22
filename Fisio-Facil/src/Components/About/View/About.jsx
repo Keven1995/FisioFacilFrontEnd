0c83f9ecfb89786b14c75cf9fe1b09adffb698ce
Keven Lucas Pereira Araujo <lkeven77@gmail.com>
2024-10-21 23:20:33 -0300

    build

--- Fisio-Facil/src/Components/About/View/About.jsx
+++ Fisio-Facil/src/Components/About/View/About.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TerapiaHome from '/src/assets/TerapiaHome.jpg';
-
+import TerapiaHome from '../../../assets/TerapiaHome.jpg';
 import Choice from '/src/assets/choice.jpg';
 import "../../Styles/Sobre.css";
 
