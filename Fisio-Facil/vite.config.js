4ff8a20e050de27757a05c5ede62d55b0983272d
Keven Lucas Pereira Araujo <lkeven77@gmail.com>
2024-10-23 19:33:03 -0300

    teste

--- Fisio-Facil/vite.config.js
+++ Fisio-Facil/vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Garante que a pasta de saída seja 'dist'
+    outDir: 'dist'
   },
-})
+  server: {
+    open: true
+  },
+  base: './',  // Para garantir que os caminhos relativos funcionem corretamente
+});
