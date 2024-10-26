dad142e48a40ba8bb14179688d64086524168f1d
Keven Lucas Pereira Araujo <lkeven77@gmail.com>
2024-10-26 09:14:04 -0300

    teste de estilo

--- Fisio-Facil/src/Modules/Servicos/View/Servicos.jsx
+++ Fisio-Facil/src/Modules/Servicos/View/Servicos.jsx
@@ -8,7 +8,6 @@ import Superiores from "../../../assets/img/superiores.jpg";
 
 const Servicos = () => {
   const navigate = useNavigate();
-  
 
   const handleClick = (path) => {
     navigate(path);
@@ -16,92 +15,53 @@ const Servicos = () => {
 
   return (
     <div className="container-servicos text-center mt-5">
-      <h1 className="mb-5" style={{ fontFamily: "Arial", fontWeight: "bold" }}>
-        FisioFacil
-      </h1>
+      <h1 className="servicos-title">Nossos Serviços</h1>
 
-      {/* Usando Flexbox do Bootstrap para os cards */}
       <div className="row justify-content-center">
-        {/* Card 1 - Membros Superiores */}
         <div className="col-lg-4 col-md-6 mb-4">
           <div
-            className="card p-3"
-            style={{
-              backgroundColor: "#a0e57b",
-              borderRadius: "20px",
-              cursor: "pointer",
-            }}
+            className="service-card"
             onClick={() => handleClick("/MembrosSuperiores")}
-            //onClick={() => handleClick("/login")}
           >
             <img
               src={Superiores}
               alt="Membros Superiores"
-              className="card-img-top"
-              style={{
-                borderRadius: "15px",
-                height: "200px",
-                objectFit: "cover",
-              }}
+              className="service-card-img"
             />
             <div className="card-body">
-              <h5 className="card-title">MEMBROS SUPERIORES</h5>
+              <h5 className="service-card-title">MEMBROS SUPERIORES</h5>
             </div>
           </div>
         </div>
 
-        {/* Card 3 - Coluna Vertebral */}
         <div className="col-lg-4 col-md-6 mb-4">
           <div
-            className="card p-3"
-            style={{
-              backgroundColor: "#a0e57b",
-              borderRadius: "20px",
-              cursor: "pointer",
-            }}
+            className="service-card"
             onClick={() => handleClick("/ColunaVertebral")}
-            //onClick={() => handleClick("/login")}
           >
             <img
               src={colunaVertebralImg}
               alt="Coluna Vertebral"
-              className="card-img-top"
-              style={{
-                borderRadius: "15px",
-                height: "200px",
-                objectFit: "cover",
-              }}
+              className="service-card-img"
             />
             <div className="card-body">
-              <h5 className="card-title">COLUNA VERTEBRAL</h5>
+              <h5 className="service-card-title">COLUNA VERTEBRAL</h5>
             </div>
           </div>
         </div>
 
-        {/* Card 2 - Membros Inferiores */}
         <div className="col-lg-4 col-md-6 mb-4">
           <div
-            className="card p-3"
-            style={{
-              backgroundColor: "#a0e57b",
-              borderRadius: "20px",
-              cursor: "pointer",
-            }}
+            className="service-card"
             onClick={() => handleClick("/MembrosInferiores")}
-            //onClick={() => handleClick("/login")}
           >
             <img
               src={Inferiores}
               alt="Membros Inferiores"
-              className="card-img-top"
-              style={{
-                borderRadius: "15px",
-                height: "200px",
-                objectFit: "cover",
-              }}
+              className="service-card-img"
             />
             <div className="card-body">
-              <h5 className="card-title">MEMBROS INFERIORES</h5>
+              <h5 className="service-card-title">MEMBROS INFERIORES</h5>
             </div>
           </div>
         </div>
