d627142ed848f121edb92b4735bf84725b815ed7
Keven Lucas Pereira Araujo <lkeven77@gmail.com>
2024-11-02 09:34:55 -0300

    Adicionado docker

--- Fisio-Facil/vite.config.js
+++ Fisio-Facil/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
   },
   server: {
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Alvo do seu backend
-        changeOrigin: true, // Altera a origem da solicitação para o alvo
+        target: process.env.VITE_API_URL || 'http://localhost:8080', // Use a variável ou localhost como fallback
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), // Remove "/api" do início do caminho
       },
     },
