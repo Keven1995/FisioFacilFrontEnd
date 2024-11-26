6c58115cb4c6de77b0258a98d08e902c8a522b35
Keven Lucas Pereira Araujo <lkeven77@gmail.com>
2024-11-25 22:48:11 -0300

    Melhorias na configuração do servidor

--- Fisio-Facil/vite.config.js
+++ Fisio-Facil/vite.config.js
@@ -1,21 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
   server: {
     open: true,
     port: 3000,
     proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'fisiofacil-backend-byeacga0d9a3d7fc.canadacentral-01.azurewebsites.net',
+      "/api": {
+        target: process.env.VITE_API_URL || "https://fisiofacil-backend-byeacga0d9a3d7fc.canadacentral-01.azurewebsites.net",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-  base: './', 
+  base: "/", // Base deve ser '/' para produção
 });
